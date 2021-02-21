4b496eb82de538857d545584a995bab0486a9b09
Ntuthuko Mpaku <ntuthuko2@live.com>
2021-02-21 22:23:20 +0200

    setting up navigation complete

--- app/src/main/java/com/freecontent/newsapp/ui/fragments/SearchNewsFragment.kt
+++ app/src/main/java/com/freecontent/newsapp/ui/fragments/SearchNewsFragment.kt
@@ -3,5 +3,5 @@ package com.freecontent.newsapp.ui.fragments
 import androidx.fragment.app.Fragment
 import com.freecontent.newsapp.R
 
-class SearchNewFragment: Fragment(R.layout.fragment_search_news) {
+class SearchNewsFragment: Fragment(R.layout.fragment_search_news) {
 }
